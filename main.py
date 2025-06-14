6c84ee53b24230a318a8ae75d094608e66b57057
Tylernxtdr <tylertrevor2009@gmail.com>
2025-06-14 15:10:57 +0300

    templates

--- main.py
+++ main.py
@@ -1,33 +1,36 @@
-from flask import Flask
+from flask import Flask,render_template
 
 #creating a flask instance
 app = Flask(__name__)
 
-@app.route('/') # decorator function -> can take saome parameters
-def home(): # view function
-    return "Trevor Tyler"
+# @app.route('/') # decorator function -> can take saome parameters
+# def home(): # view function
+#     return render_template("index.html")
 
-@app.route('/name') #rule -> defines a path a user accesses in the browser
-def name(): # view function
-    return "My name is Tyler"
+@app.route('/') #rule -> defines a path a user accesses in the browser & endpoint - name of view function
+def product(): # view function
+    product = "rice"
+    return render_template("products.html",data = product)
 
-app.run(debug=True)  # watcher - checkig for any changes in an application and reloading it  
 
-# # TASK 
-#1. Create 3 routes and return 3 different data values and test them in yur browser
-@app.route('/num0')
-def fun1():
-    return "PRODUCT1"
+@app.route('/sales')
+def sale():
+    sale = 200
+    return render_template("sales.html",data2 = sale)
 
-@app.route('/num1')
-def fun2():
-    return "PRODUCT2"
+@app.route('/dashboard')
+def dboard():
+    db = "My Dashboard"
+    return render_template("dashboard.html",data3=db)
 
-@app.route('/num2')
-def fun3():
-    return "PRODUCT3"
+@app.route('/login')
+def login():
+    lg = "Log into your account"
+    return render_template("login.html",data4=lg)
+
+@app.route('/register')
+def register():
+    rg = "Register a new account"
+    return render_template("register.html",data5 = rg)
 
 app.run(debug=True)
-#2. In your project folder create 2 folders :
-   # a) templates
-   # b) static
